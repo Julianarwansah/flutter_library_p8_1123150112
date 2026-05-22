37ef40c1d3c840737e7275035f88bae88d75b803
Julian Arwansah <julianarwansahh@gmail.com>
2026-05-22 17:44:11 +0700

    update title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Test Package App',
+      title: 'package julian',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
